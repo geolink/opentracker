b585893cc5a8dda65777302e457f000dc69e0032
Mark Wadham <m@rkw.me>
2015-05-19 09:34:53 +0100

    bugfix and stdout logging

--- daemon/daemon.rb
+++ daemon/daemon.rb
@@ -14,13 +14,15 @@ while (session = server.accept)
     Thread.start do
         input = session.gets
 
+        puts "#{input}"
+
         m = input.match(/^([0-9a-zA-Z]+),([0-9\.\-]+),([0-9\.\-]+),([0-9\.\-]+),([0-9\.\-]+),([0-9\.\-]+),([0-9]+),([0-9]+),([0-9\.]+),([01]),([0-9]+)/);
         if m[1] == $key
             con = Mysql.new $mysql_host, $mysql_user, $mysql_pass, $mysql_db
 
             ts = Time.now.strftime("%Y-%m-%d %H:%M:%S");
 
-            con.query("INSERT into `log` (timestamp,lat,lon,speed,altitude,heading,hdop,satellites,vehicleBat,engineStatus,runningTime,ip) values ('#{ts}','#{m[2]}','#{m[3]}','#{m[4]}','#{m[5]}','#{m[6]}','#{m[7]}','#{m[8]}','#{m[9]}','#{session.peeraddr[2]}');");
+            con.query("INSERT into `log` (timestamp,lat,lon,speed,altitude,heading,hdop,satellites,vehicleBat,engineStatus,runningTime,ip) values ('#{ts}','#{m[2]}','#{m[3]}','#{m[4]}','#{m[5]}','#{m[6]}','#{m[7]}','#{m[8]}','#{m[9]}','#{m[10]}','#{m[11]}','#{session.peeraddr[2]}');");
         end
         session.close
     end
