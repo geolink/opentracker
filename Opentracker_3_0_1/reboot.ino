4ef2a301fc5cfc703ace2df4143e7caee522ad46
ppescher <p.messina@robotechsrl.com>
2016-03-31 16:45:39 +0200

    changed "hard" reboot to "soft" reboot (kill external hw and reset mcu)

--- Opentracker_3_0_1/reboot.ino
+++ Opentracker_3_0_1/reboot.ino
@@ -2,24 +2,29 @@
 void reboot() {
   debug_print(F("reboot() started"));
 
-  //reboot only works with normal power, without programming cable connected
-  //turn off modem, GPS
-  gsm_on_off();
-  delay(1000);
+  //reset GPS
+  gps_off();
 
-  gsm_get_reply(0);
-
-  //turn off GPS
-  digitalWrite(PIN_STANDBY_GPS, HIGH);
-
-  //wait 4 sec
-  delay(4000);
+  //emergency power off GSM
+  gsm_off(1);
 
   //power cycle everything
   debug_print(F("Power cycling hardware."));
 
-  digitalWrite(PIN_C_REBOOT, HIGH);
-  delay(10000);  //this should never be executed
+  //disable USB to allow reboot
+  //serial monitor on the PC won't work anymore if you don't close it before reset completes
+  //otherwise, close the serial monitor, detach the USB cable and connect it again
 
-  setup();   //in case reboot failed, run setup() and continue;
+  // Console.end() does nothing, manually disable USB
+  USBDevice.detach(); // detach from Host
+
+  cpu_irq_disable();
+
+  rstc_start_software_reset(RSTC);
+  for (;;)
+  {
+    // If we do not keep the watchdog happy and it times out during this wait,
+    // the reset reason will be wrong when the board starts the next time around.
+    WDT_Restart(WDT);
+  }
 }
